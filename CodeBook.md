1394c41f63e83f8024d9fdb058992dcdcf9c917f
Fitrianingrum <Fitrianingrum>
2017-02-20 05:40:39 +0700

    completed run_analysis.R

--- CodeBook.md
+++ CodeBook.md
@@ -1,2 +1,34 @@
-# Code Book
+# CodeBook
 This code book describes the variables, the data, and any transformations or work that performed to clean up the data. 
+
+## Data source
+The dataset that is used in this project can be downloaded [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) [59.7 MB], and the brief description of it can be found [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones). 
+
+Licence:
+
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
+
+## Variables
+List of variables:
+* **subject**, ID the subject who performed the activity for each window sample, ranging from 1 to 30.
+* **activity**, activity names, from 'activity_labels.txt'
+* **features**, from 'features.txt' (total:561)    
+
+## Transformation
+1. Merge the loaded training data and test data to create one dataset
+2. Filter only the measurements on the mean and standard deviation for each measurement
+3. Change activities in the dataset to use descriptive names listed in 'activity_labels.txt'
+4. Label the dataset with descriptive names, e.g.:
+   * prefix t is replaced by time
+   * Acc is replaced by Accelerometer
+   * Gyro is replaced by Gyroscope
+   * prefix f is replaced by frequency
+   * Mag is replaced by Magnitude
+   * BodyBody is replaced by Body
+   * () is replaced by empty string
+5. Output the processed dataset into an independent tidy dataset.
+Please see run_analysis.R for detailed transformation.
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,61 @@
+# A helper method to check dependency packages, install it if necessary 
+InstallPackagesIfNotExist <- function(...) {
+  lapply(list(...), function(lib) {
+    if (!lib %in% installed.packages()) 
+      install.packages(lib)
+  })
+}
+
+# Get the dataset
+if (is.na(file.info('UCI HAR Dataset')$isdir)) {
+  dataFile <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+  download.file(dataFile, 'dataset.zip')
+  unzip('dataset.zip')
+}
+
 # Merges the training and the test sets to create one data set.
+subject.train <- read.table('UCI HAR Dataset/train/subject_train.txt', header = FALSE)
+subject.test <- read.table('UCI HAR Dataset/test/subject_test.txt', header = FALSE)
+subject.all <- rbind(subject.train, subject.test)
+
+activity.train <- read.table('UCI HAR Dataset/train/y_train.txt', header = FALSE)
+activity.test <- read.table('UCI HAR Dataset/test/y_test.txt', header = FALSE)
+activity.all <- rbind(activity.train, activity.test)
+
+feature.train <- read.table('UCI HAR Dataset/train/X_train.txt', header = FALSE)
+feature.test <- read.table('UCI HAR Dataset/test/X_test.txt', header = FALSE)
+feature.all <- rbind(feature.train, feature.test)
+
+names(subject.all) <- c("subject")
+names(activity.all) <- c("activity")
+
+features.name <- read.table('UCI HAR Dataset/features.txt', header = FALSE)
+names(feature.all) <- features.name$V2
+
+data = cbind(subject.all, activity.all)
+data.all = cbind(data, feature.all)
 
 # Extracts only the measurements on the mean and standard deviation for each measurement.
+features.selected <- features.name$V2[grep("mean\\(\\)|std\\(\\)", features.name$V2)]
+columns <- c("subject", "activity", as.character(features.selected))
+data.selected <- subset(data.all, select=columns)
 
 # Uses descriptive activity names to name the activities in the data set
+activity.names <- read.table('UCI HAR Dataset/activity_labels.txt', header = FALSE)
+data.selected$activity <- activity.names[data.selected[,2],2]
 
-# Appropriately labels the data set with descriptive variable names.
+# Appropriately labels the data set with descriptive variable names
+names(data.selected)<-gsub("^t", "time", names(data.selected))
+names(data.selected)<-gsub("^f", "frequency", names(data.selected))
+names(data.selected)<-gsub("Acc", "Accelerometer", names(data.selected))
+names(data.selected)<-gsub("Gyro", "Gyroscope", names(data.selected))
+names(data.selected)<-gsub("Mag", "Magnitude", names(data.selected))
+names(data.selected)<-gsub("BodyBody", "Body", names(data.selected))
+names(data.selected)<-gsub("\\(|\\)", "", names(data.selected))
 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+InstallPackagesIfNotExist("plyr")
+data.tidy<-aggregate(. ~subject + activity, data.selected, mean)
+data.tidy<-data.tidy[order(data.tidy$subject,data.tidy$activity),]
+write.table(data.tidy, file = "tidy_data.txt", row.name=FALSE)
+ 
